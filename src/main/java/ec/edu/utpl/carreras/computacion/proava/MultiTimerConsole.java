1dab47fca74c77eeb1045c9c36ad743e94e08f9f
andresvallejoT-T <andresvallejo102006@gmail.com>
2026-04-23 09:54:07 -0500

    Analisis

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -6,7 +6,10 @@ package ec.edu.utpl.carreras.computacion.proava;
 public class MultiTimerConsole {
     public static void main(String[] args) {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
+        TimerTask timerB = new TimerTask("Temporizador B", 3);
+
         timerA.run();
+        timerB.run();
         System.out.println();
     }
 }
