02d9b40d8c41d24c98d96318c028e773e7b4152b
andresvallejoT-T <andresvallejo102006@gmail.com>
2026-04-23 09:15:26 -0500

    Parte 2:Alternativa B — implements Runnable

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -6,7 +6,25 @@ package ec.edu.utpl.carreras.computacion.proava;
 public class MultiTimerConsole {
     public static void main(String[] args) {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
-        System.out.println();
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        TimerTask timerC = new TimerTask("Temporizador C", 3);
+
+        Thread hiloA = new Thread(() -> timerA.run(), "Hilo-A");
+        Thread hiloB = new Thread(() -> timerB.run(), "Hilo-B");
+        Thread hiloC = new Thread(() -> timerC.run(), "Hilo-C");
+
+        hiloA.start();
+        hiloB.start();
+        hiloC.start();
+
+        try {
+            hiloA.join(); // espera a que Hilo-A termine
+            hiloB.join(); // espera a que Hilo-B termine
+            hiloC.join();  // espera a que Hilo-C termine
+        } catch (InterruptedException e) { Thread.currentThread().interrupt();
+        }
+        System.out.println(">>> Todos los temporizadores han finalizado.");
+
     }
+
 }
